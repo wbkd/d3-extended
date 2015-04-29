2da601efa48ad7af23a67daf8d22eb4991211330
Christopher <chrtze@gmail.com>
2015-04-29 20:28:22 +0200

    add node module

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    footer = require('gulp-footer'),
+    header = require('gulp-header'),
     del = require('del');
 
 gulp.task('clean', function() {
   del('dist');
+  del('lib');
 });
 
 gulp.task('d3-plugin', function() {
   return gulp.src('src/**')
     .pipe(concat('d3-query.js'))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'))
 });
 
 gulp.task('d3-plugin-minified', function() {
@@ -20,4 +24,12 @@ gulp.task('d3-plugin-minified', function() {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', ['clean', 'd3-plugin', 'd3-plugin-minified'])
+gulp.task('node-module', function() {
+  return gulp.src('src/**')
+    .pipe(concat('d3-query.js'))
+    .pipe(header('var d3 = require("d3");\n\n'))
+    .pipe(footer('\n\nmodule.exports = d3;'))
+    .pipe(gulp.dest('lib/'))
+});
+
+gulp.task('build', ['clean', 'd3-plugin', 'd3-plugin-minified', 'node-module']);
