6c7cc4029b927972acd9c6c6cd6ca7ddafb99846
moklick <info@moritzklack.com>
2015-05-18 14:01:57 +0200

    fix last function bug

--- d3-extended.js
+++ d3-extended.js
@@ -88,8 +88,8 @@ d3.selection.prototype.hide = function() {
 }
 d3.selection.prototype.last = function() {
   // adapted from https://github.com/mbostock/d3/blob/master/src/selection/each.js
-  for (var j = this.length; j >= 0; j--) {
-      for (var group = this[j], i = group.length, node; i >= 0; i--) {
+  for (var j = this.length - 1; j >= 0; j--) {
+      for (var group = this[j], i = group.length - 1, node; i >= 0; i--) {
             if (node = group[i]) return d3.select(node);
       }
   }
