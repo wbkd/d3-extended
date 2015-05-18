6fa4f0b46221407133a8e91186073b222c847c2d
moklick <info@moritzklack.com>
2015-05-18 13:57:04 +0200

    return a d3 selection in last and first functions

--- d3-extended.js
+++ d3-extended.js
@@ -74,7 +74,7 @@ d3.selection.prototype.first = function() {
     // adapted from https://github.com/mbostock/d3/blob/master/src/selection/each.js
     for (var j = 0, m = this.length; j < m; j++) {
         for (var group = this[j], i = 0, n = group.length, node; i < n; i++) {
-              if (node = group[i]) return node;
+              if (node = group[i]) return d3.select(node);
         }
     }
 }
@@ -90,7 +90,7 @@ d3.selection.prototype.last = function() {
   // adapted from https://github.com/mbostock/d3/blob/master/src/selection/each.js
   for (var j = this.length; j >= 0; j--) {
       for (var group = this[j], i = group.length, node; i >= 0; i--) {
-            if (node = group[i]) return node;
+            if (node = group[i]) return d3.select(node);
       }
   }
 }
--- src/core/last.js
+++ src/core/last.js
@@ -2,7 +2,7 @@ d3.selection.prototype.last = function() {
   // adapted from https://github.com/mbostock/d3/blob/master/src/selection/each.js
   for (var j = this.length; j >= 0; j--) {
       for (var group = this[j], i = group.length, node; i >= 0; i--) {
-            if (node = group[i]) return node;
+            if (node = group[i]) return d3.select(node);
       }
   }
 }
