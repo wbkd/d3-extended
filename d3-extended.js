0de47e429d2138d127936c0e569f2e2f2ed0b4e4
chrtze <chrtze@gmail.com>
2015-07-13 17:57:04 +0200

    remove trigger test

--- d3-extended.js
+++ d3-extended.js
@@ -155,7 +155,19 @@ d3.selection.prototype.toggleClass = function(className) {
   this.classed(className, !this.classed(className));
   return this;
 }
+d3.selection.prototype.trigger = function(evtName, data) {
+  //we now use the jQuery implementation as d3.selection.on is used differently.
+  if (window.CustomEvent) {
+    var event = new CustomEvent(evtName, {detail: data});
+  } else {
+    var event = document.createEvent('CustomEvent');
+    event.initCustomEvent(evtName, true, true, data);
+  }
 
+  this.node().dispatchEvent(event);
+
+  return this;
+}
 
 return d3;
 }
